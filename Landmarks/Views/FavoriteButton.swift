0b0d31a8f112f1e983a12db1fc8ed2e8b0373bea
John Kouris <jdkouris@gmail.com>
2021-05-04 09:12:09 -0700

    Added FavoriteButton file to the project

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by John Kouris on 5/4/21.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton()
+    }
+}
