4a288b1417257423b96db2b91279f422127d86dd
John Kouris <jdkouris@gmail.com>
2021-05-05 09:00:42 -0700

    Added the RotatedBadgeSymbol file to the project

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by John Kouris on 5/5/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol()
+    }
+}
