9cb1196fe4d0a55d396aa0b19bb210087d446077
John Kouris <jdkouris@gmail.com>
2021-05-08 11:38:12 -0700

    Configured the ProfileSummary and ProfileHost

--- Landmarks/Profiles/ProfileSummary.swift
+++ Landmarks/Profiles/ProfileSummary.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+    var profile: Profile
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Text("Notifications: \(profile.prefersNotifications ? "On" : "Off")")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+            }
+        }
     }
 }
 
 struct ProfileSummary_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileSummary()
+        ProfileSummary(profile: Profile.default)
     }
 }
