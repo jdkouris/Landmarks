603988a3271f33b70df55084d3261238fa078e9d
John Kouris <jdkouris@gmail.com>
2021-05-08 11:32:55 -0700

    Configured the Profile struct

--- Landmarks/Model/Profile.swift
+++ Landmarks/Model/Profile.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "j_kouris")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case sprint = "🌷"
+        case summer = "☀️"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { self.rawValue }
+    }
+}
