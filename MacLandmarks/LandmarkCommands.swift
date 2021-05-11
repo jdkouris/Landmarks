7ba3341b74d3cda4905da6a178173a4e873eb704
John Kouris <jdkouris@gmail.com>
2021-05-11 07:36:57 -0700

    Added a Landmark menu item to mark favorites

--- MacLandmarks/LandmarkCommands.swift
+++ MacLandmarks/LandmarkCommands.swift
@@ -8,7 +8,30 @@
 import SwiftUI
 
 struct LandmarkCommands: Commands {
+    private struct MenuContent: View {
+        @FocusedBinding(\.selectedLandmark) var selectedLandmark
+
+        var body: some View {
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
+    }
+
     var body: some Commands {
         SidebarCommands()
+    }
+}
+
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue }
     }
 }
