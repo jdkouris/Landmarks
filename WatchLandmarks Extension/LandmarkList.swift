97843c8e22500027a7bb01712d3ee1cfedc27fc1
John Kouris <jdkouris@gmail.com>
2021-05-11 07:10:07 -0700

    Added new LandmarkList file for watch extension

---
+++ WatchLandmarks Extension/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  WatchLandmarks Extension
+//
+//  Created by John Kouris on 5/11/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
