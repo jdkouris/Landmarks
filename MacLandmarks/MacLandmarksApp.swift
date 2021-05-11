09d3dfec18464d8c9d348223989f5a396946cf47
John Kouris <jdkouris@gmail.com>
2021-05-11 06:48:24 -0700

    Added the Mac target for Landmarks

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by John Kouris on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
