1629c8ce020839e4607fab85e8527c76491934c3
John Kouris <jdkouris@gmail.com>
2021-05-11 07:27:38 -0700

    Added SidebarCommands to new LandmarkCommands file and call it in LandmarksApp

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by John Kouris on 5/11/21.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
